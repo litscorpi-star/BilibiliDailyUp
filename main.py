a9ea0413d2c256cd28132db4b4c96641800bcb9a
litscorpi <79298405+litscorpi-star@users.noreply.github.com>
2024-11-04 18:08:30 +0800

    Update main.py
    获取用户视频列表

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ Project Address: https://github.com/Wyatt1026/BilibiliDailyUp
 from core.bilibili import Bilibili
 from config import config
 from os import environ
+import ramdom
 
 if __name__ == '__main__':
-    if not config.USE_ENVIRONMENT_VARIABLE:
-        ck_list = config.COOKIE_LIST
-        for ck in ck_list:
-            bilibili = Bilibili(ck)
-            bilibili.go()
-    else:
-        ck = environ.get('BILIBILI')
-        if not ck:
-            print("未设置BILIBILI这个环境变量 任务终止")
-        else:
-            bilibili = Bilibili(ck)
-            bilibili.go()
+    ck_list = config.COOKIE_LIST
+    # for ck in ck_list:
+    ck = ramdom.choice(ck_list)
+    # 获取指定用户的视频列表，mid 为用户id count 为获取的视频数量,默认值为30
+    bilibili = Bilibili(ck)
+    # 去看剧
+    # mid = '3546766586678158'
+    # 白色苹果冻
+    # mid = '1794984662'
+    # litscorpi
+    mid = '431316421'
+    count = 100
+    bvids_list = bilibili.my_video_list(mid, count)
+    print(bvids_list)
