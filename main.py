bede712f6d4c41456f18723e2a96da41da64e527
litscorpi <79298405+litscorpi-star@users.noreply.github.com>
2024-11-04 18:11:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ Project Address: https://github.com/Wyatt1026/BilibiliDailyUp
 from core.bilibili import Bilibili
 from config import config
 from os import environ
-import ramdom
+import random
 
 if __name__ == '__main__':
     ck_list = config.COOKIE_LIST
     # for ck in ck_list:
-    ck = ramdom.choice(ck_list)
+    ck = random.choice(ck_list)
     # 获取指定用户的视频列表，mid 为用户id count 为获取的视频数量,默认值为30
     bilibili = Bilibili(ck)
     # 去看剧
