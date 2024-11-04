8a619982ce104275fb3e8846bd7640f321eb6d2d
litscorpi <79298405+litscorpi-star@users.noreply.github.com>
2024-11-04 19:49:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     ck_list = config.COOKIE_LIST
     # for ck in ck_list:
     ck = random.choice(ck_list)
-    # 获取指定用户的视频列表，mid 为用户id count 为获取的视频数量,默认值为30
+    # 获取指定用户的视频列表，mid 为用户id
+    # 修改mid即可获取该用户的 bvids 列表
     bilibili = Bilibili(ck)
     # 去看剧
     # mid = '3546766586678158'
@@ -21,6 +22,6 @@ if __name__ == '__main__':
     # mid = '1794984662'
     # litscorpi
     mid = '431316421'
-    count = 100
-    bvids_list = bilibili.my_video_list(mid, count)
+    count = 30
+    bvids_list = bilibili.user_video_list(mid, count)
     print(bvids_list)
